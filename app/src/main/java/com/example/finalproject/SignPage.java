7146b96dd70245df14006b298fb32f7117577047
Huu-Khang Nguyen <65224957+khangnguyen197@users.noreply.github.com>
2020-12-21 06:15:57 +0700

    Login Page, Sign Page
    fix Login Page
    add Sign Page
    do not touch home Page

--- app/src/main/java/com/example/finalproject/SignPage.java
+++ app/src/main/java/com/example/finalproject/SignPage.java
@@ -1,32 +1,36 @@
 package com.example.finalproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.method.LinkMovementMethod;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class LoginPage extends AppCompatActivity {
+public class SignPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_page);
+        setContentView(R.layout.activity_sign_page);
         Globals globals = new Globals();
         globals.transStatus(getWindow());
         onClick();
     }
 
     void onClick() {
-        TextView signUpActivity = (TextView) findViewById(R.id.sign_link);
+        TextView signUpActivity = (TextView) findViewById(R.id.login_link);
         signUpActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent signPage = new Intent(LoginPage.this, SignPage.class);
-                startActivity(signPage);
+                Intent logPage = new Intent(SignPage.this, LoginPage.class);
+                startActivity(logPage);
             }
         });
     }
