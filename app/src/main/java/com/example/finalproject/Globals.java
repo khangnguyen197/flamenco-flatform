78f943e1be57a2280e3281585d777b2e2b5cb667
Huu-Khang Nguyen <65224957+khangnguyen197@users.noreply.github.com>
2021-01-16 18:14:59 +0700

    fix keyboard push

--- app/src/main/java/com/example/finalproject/Globals.java
+++ app/src/main/java/com/example/finalproject/Globals.java
@@ -1,6 +1,7 @@
 package com.example.finalproject;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -8,23 +9,22 @@ import android.view.Window;
 import android.view.WindowManager;
 
 
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Globals extends AppCompatActivity {
     public void transStatus(Window w) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
             w.getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            w.setStatusBarColor(Color.TRANSPARENT);
         }
-        }
+    }
 
-    public String setText(){
+    public String setText() {
         return "test";
     }
 }
