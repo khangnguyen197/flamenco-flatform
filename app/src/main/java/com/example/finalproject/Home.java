5f28779b4c3e6d8a1ff83d87456991e0b1c4f1f9
Huu-Khang Nguyen <65224957+khangnguyen197@users.noreply.github.com>
2021-01-19 01:31:55 +0700

    add basic Menu

--- app/src/main/java/com/example/finalproject/Home.java
+++ app/src/main/java/com/example/finalproject/Home.java
@@ -1,7 +1,10 @@
 package com.example.finalproject;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -9,10 +12,15 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 
+import com.google.android.material.navigation.NavigationView;
+
 import java.util.ArrayList;
 
 public class Home extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +36,23 @@ public class Home extends AppCompatActivity {
         content.setLayoutManager(new LinearLayoutManager(this));
         //content.setAdapter(HotelAdapter(hotelList));
 
+    }
+
+    private void menuAction (){
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        /**Tool Bar */
+        setSupportActionBar(toolbar);
+
+        /**Navigation Drawer*/
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
     }
+
+
 
 }
