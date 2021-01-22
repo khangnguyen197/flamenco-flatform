80bfe551f99c0a66c2ff6027959a4897bcdae920
manhquandao <52850452+manhquandao@users.noreply.github.com>
2021-01-22 10:10:12 +0700

    Update AboutUs.java

--- app/src/main/java/com/example/finalproject/AboutUs.java
+++ app/src/main/java/com/example/finalproject/AboutUs.java
@@ -10,5 +10,7 @@ public class AboutUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        Globals globals=new Globals();
+        globals.transStatus(getWindow());
     }
 }
