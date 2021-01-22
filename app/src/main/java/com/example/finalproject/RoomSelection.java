b5e4cde2bbe20c1a5ffb1acb050401935a39e6ab
Huu-Khang Nguyen <65224957+khangnguyen197@users.noreply.github.com>
2021-01-22 17:24:08 +0700

    fix x n

--- app/src/main/java/com/example/finalproject/RoomSelection.java
+++ app/src/main/java/com/example/finalproject/RoomSelection.java
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
